a826f9aedec1c0f86c72a23396e9533efb85e2b8
itsmattpaw <itsmattpaw@gmail.com>
2022-03-07 13:09:50 -0500

    commit tree ff459addc0a61b03a1c806cd3c920037dc75b71f

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,39 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css(".student-card").each do |st|
+      students << {
+        name: st.css("h4").text,
+        location: st.css("p").text,
+        profile_url: st.css("a").attribute('href').text
+      }
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile = {}
+    doc = Nokogiri::HTML(open(profile_url))
+    profile = {
+      profile_quote: doc.css(".profile-quote").text,
+      bio: doc.css(".description-holder p").text
+    }
+    social = doc.css(".social-icon-container a")
+    social.each do |img|
+      case img.css(".social-icon").attribute('src').text
+      when "../assets/img/twitter-icon.png"
+        profile.store(:twitter, img.attribute('href').text)
+      when "../assets/img/linkedin-icon.png"
+        profile.store(:linkedin, img.attribute('href').text)
+      when "../assets/img/github-icon.png"
+        profile.store(:github, img.attribute('href').text)
+      when "../assets/img/rss-icon.png"
+        profile.store(:blog, img.attribute('href').text)
+      end
+    end
+    profile
   end
 
 end
